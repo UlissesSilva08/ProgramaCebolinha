80c677f17a03b28df14dca33a3c25baf537295ad
UlissesSilva08 <ulissesmasterchief@gmail.com>
2021-08-29 15:06:28 -0300

    Inserindo o comando .Replace() para inverter os caracteres obtido pelo usuário via string

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProgramaCebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oi, me chamo Cebolinha!");
+            Console.WriteLine("  \\|/");
+            Console.WriteLine("(^ - ^)");
+
+            Console.WriteLine();
+
+            Console.Write("Digite seu texto para eu ler: ");
+            string textoTrocado = Console.ReadLine();
+            
+            Console.WriteLine();
+
+            Console.Write("OK, vou ler aqui: ");
+            Console.WriteLine(textoTrocado.Replace("r" , "l" ).Replace("R" , "L") + ", celto?");
+        }
+    }
+}
